b265900bf9e5696cc2f2ddc0b83e895b3e059836
Shem Nguyen <shemit@gmail.com>
2014-01-24 21:41:19 -0800

    Adding Solutions

--- Scripts/NetworkManager.cs
+++ Scripts/NetworkManager.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
 using System.Collections;
 
-public class NetworkManager : MonoBehaviour
-{
+public class NetworkManager : MonoBehaviour {
+
+	float botX = 0.1F;
+	float botY = 0.1F;
+	float botW = 0.1F;
+	float botH = 0.1F;
 
 	// Use this for initialization
-	void Start ()
-	{
-	
+	void Start () {
 	}
 	
 	// Update is called once per frame
-	void Update ()
-	{
-	
+	void Update () {
+		
 	}
+
+	void OnGUI() {
+		Rect r1 = new Rect(this.botX, this.botY, this.botH, this.botW);
+		Rect r2 = new Rect)
 }
 
