3e0a0ec841c5671903555e97180a7ad12dcdbaf3
Jeremy <utilityjerm@gmail.com>
2014-01-25 01:48:55 -0800

    got asteroid spawning pretty much in

---
+++ Assets/Resources/Asteroid.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour {
+
+  private string id;
+  private GameObject asteroid;
+  private Vector2 pos;
+  private Vector2 vel;
+  private Rigidbody asteroidRigidBody;
+
+  // Use this for initialization
+  void Start () {
+    Debug.Log("AsteroidSpawned");
+    this.asteroid = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    this.asteroidRigidBody = asteroid.AddComponent<Rigidbody>(); // Add the rigidbody.
+    this.asteroidRigidBody.mass = 5; // Set the asteroid's mass to 5 via the Rigidbody.
+    this.asteroidRigidBody.useGravity = false;
+    this.asteroidRigidBody.constraints = RigidbodyConstraints.FreezePositionZ;
+
+    this.asteroidRigidBody.position = new Vector3(this.pos.x, this.pos.y);
+    this.asteroidRigidBody.velocity = new Vector3(this.vel.x, this.vel.y);
+  }
+  
+  // Update is called once per frame
+  void Update () {
+
+  }
+
+  public void init (string initId, Vector2 initPos, Vector2 initVel) {
+    this.id = initId;
+    this.pos = initPos;
+    this.vel = initVel;
+  }
+}
