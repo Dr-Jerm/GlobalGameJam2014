fb252a9aba2b67bf95082a9be3a3bb628244a9fa
Jeremy <utilityjerm@gmail.com>
2014-01-24 22:12:27 -0800

    started asteroidSpawner

---
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidSpawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	 Debug.Log("hello world");
+   
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	 
+	}
+
+  void spawnAsteroid (Vector2 pos, Vector2 vel) {
+    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    cube.transform.position = new Vector3(pos.x, pos.y, 0);
+
+  }
+}
