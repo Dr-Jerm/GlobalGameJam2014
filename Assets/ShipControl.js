6cc79c11388192a79a2bc9598287c30a4da31541
unknown <egeusz@gmail.com>
2014-01-25 00:56:15 -0700

    spaceship controls YAY!

---
+++ Assets/ShipControl.js
@@ -0,0 +1,17 @@
+﻿
+var thrustspeed = 500; 
+var turnspeed = 50; 
+var turnfriction = 2; 
+function Update () {
+	
+	var inputVert = Input.GetAxis("Vertical");
+	
+	rigidbody.AddRelativeTorque (0,0,(Input.GetAxis("Horizontal"))*thrustspeed*Time.deltaTime);
+	rigidbody.AddRelativeForce(0,inputVert*turnspeed*Time.deltaTime,0);
+  	
+ 	
+	if (rigidbody.angularVelocity.magnitude > 0) 
+	{
+    	rigidbody.AddRelativeTorque (rigidbody.angularVelocity.normalized*-turnfriction);
+	}    
+}
