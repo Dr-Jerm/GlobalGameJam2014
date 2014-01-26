716d5a3ddcd0b19eea2929b589c6eed8469a0e6a
Brian Tran <b16tran@gmail.com>
2014-01-25 21:54:55 -0800

    no message

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	private Ray ray;
+	private RaycastHit rayCastHit;
+	
+	void Awake()
+	{
+	}
+	
+	void Update ()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			
+			if(Physics.Raycast (ray, out rayCastHit))
+			{
+				if(rayCastHit.transform.name == "playButton")
+					Application.LoadLevel("game");
+			}
+		}
+	}
+}
