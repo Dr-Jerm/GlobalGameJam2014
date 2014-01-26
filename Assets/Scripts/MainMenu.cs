0c719e4539c2854997405d6a643b2d16a8f1faa2
Eileen <eeb.misc@gmail.com>
2014-01-26 00:33:15 -0800

    fixing...basically ignore this

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,8 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
-using Facebook.MiniJSON;
-using System;
 
 public class MainMenu : MonoBehaviour {
 	public GameObject sound;
@@ -10,24 +7,10 @@ public class MainMenu : MonoBehaviour {
 	private Ray ray;
 	private RaycastHit rayCastHit;
 
-	private static List<object>                 friends         = null;
-	private static Dictionary<string, string>   profile         = null;
-	private bool    haveUserPicture       = false;
-
-	enum LoadingState 
-	{
-		WAITING_FOR_INIT,
-		WAITING_FOR_INITIAL_PLAYER_DATA,
-		DONE
-	};
-	
-	private LoadingState loadingState = LoadingState.WAITING_FOR_INIT;
-	
 	void Awake()
 	{
 		// Initialize FB SDK              
 		enabled = false;
-		Debug.Log ("test");
 		FB.Init(SetInit, OnHideUnity);  
 	}
 	
@@ -71,7 +54,7 @@ public class MainMenu : MonoBehaviour {
 		if (FB.IsLoggedIn)                                                                       
 		{                                                                                        
 			FbDebug.Log("Already logged in");                                                    
-			OnLoggedIn();                                                                        
+			//OnLoggedIn();                                                                        
 		}                                                                                        
 	}                                                                                            
 
@@ -88,61 +71,5 @@ public class MainMenu : MonoBehaviour {
 			// start the game back up - we're getting focus again                                
 			Time.timeScale = 1;                                                                  
 		}                                                                                        
-	}
-
-	void OnLoggedIn()
-	{
-		FbDebug.Log("Logged in. ID: " + FB.UserId);
-		
-		// Reqest player info and profile picture
-		FB.API("/me?fields=id,first_name,friends.limit(100).fields(first_name,id)", Facebook.HttpMethod.GET, APICallback);
-		//TODO: put these in
-		//FB.API(Util.GetPictureURL("me", 128, 128), Facebook.HttpMethod.GET, MyPictureCallback);
-		// Load high scores
-		//QueryScores();
-	}
-
-	void APICallback(FBResult result)
-	{
-		FbDebug.Log("APICallback");
-		if (result.Error != null)
-		{
-			FbDebug.Error(result.Error);
-			// Let's just try again
-			FB.API("/me?fields=id,first_name,friends.limit(100).fields(first_name,id)", Facebook.HttpMethod.GET, APICallback);
-			return;
-		}
-		
-		profile = Util.DeserializeJSONProfile(result.Text);
-		//GameStateManager.Username = profile["first_name"];
-		friends = Util.DeserializeJSONFriends(result.Text);
-		checkIfUserDataReady();
-	}
-
-	void MyPictureCallback(FBResult result)
-	{
-		FbDebug.Log("MyPictureCallback");
-		
-		if (result.Error != null)
-		{
-			FbDebug.Error(result.Error);
-			// Let's just try again
-			FB.API(Util.GetPictureURL("me", 128, 128), Facebook.HttpMethod.GET, MyPictureCallback);
-			return;
-		}
-		
-		//GameStateManager.UserTexture = result.Texture;
-		haveUserPicture = true;
-		checkIfUserDataReady();
-	}
-
-	void checkIfUserDataReady()
-	{
-		FbDebug.Log("checkIfUserDataReady");
-		//if (loadingState == LoadingState.WAITING_FOR_INITIAL_PLAYER_DATA && haveUserPicture && !string.IsNullOrEmpty(GameStateManager.Username))
-		//{
-		//	FbDebug.Log("user data ready");
-		//	loadingState = LoadingState.DONE;
-		//}
 	}
 }
