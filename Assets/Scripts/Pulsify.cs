60be115b3fcd322667066ae54c5e9aed13baf36c
Shem Nguyen <shemit@gmail.com>
2014-01-26 14:37:51 -0800

    adding the scene w/ stars

---
+++ Assets/Scripts/Pulsify.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pulsify : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Light l = this.GetComponent<Light>();
+		Debug.Log (l);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
